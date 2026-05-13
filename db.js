b62dba6239d382991f9405b768e43df8d7c353a0
Tador-Toolbox <ort@tador.com>
2026-05-13 23:10:23 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -54,6 +54,7 @@ const InstallerSchema = new mongoose.Schema({
   badge:          { type: String, default: 'מומחה מוסמך ומנוסה' },
   checklistItems: [{ type: String }],
   trustItems:     [{ type: String }],
+  template:       { type: String, enum: ['white','dark','red'], default: 'white' },
 
   // Content
   portfolioImages: [PortfolioImageSchema],
